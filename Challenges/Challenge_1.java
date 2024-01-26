75ef34974801111da1ed7643f4c24a8ef261be33
vikasrathod163 <vikasrathod010603@gmail.com>
2024-01-26 16:30:22 +0530

    challenge 1,2,3 done

---
+++ Challenges/Challenge_1.java
@@ -0,0 +1,7 @@
+package Java.Challenges;
+
+public class Challenge_1 {
+    public static void main(String[] args) {
+        System.out.println("Good Morning........");
+    }
+}
---
+++ Challenges/Challenge_3.java
@@ -0,0 +1,29 @@
+package Java.Challenges;
+
+public class Challenge_3 {
+    public static void main(String[] args) {
+        /*Patterns without loops */
+
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+        System.out.println();
+
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+
+        System.out.println();
+
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+}
