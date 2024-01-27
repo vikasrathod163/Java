f05eb44eb4a091b58edba033b9635807eca66404
vikasrathod163 <vikasrathod010603@gmail.com>
2024-01-28 01:24:58 +0530

    arithmetic operators used in java file

--- Codes/Operators/arithmetic.java
+++ Codes/Operators/arithmetic.java
@@ -1,7 +1,25 @@
 package Java.Codes.Operators;
 
+import java.util.Scanner;
+
 public class arithmetic {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        int n1;
+        int n2;
+
+        System.out.println("Enter 1st number : ");
+        n1 = sc.nextInt();
+        System.out.println("Enter 2nd number : ");
+        n2 = sc.nextInt();
+
+        int sum = n1+n2;
+        int sub = n1-n2;
+        int mul = n1*n2;
+        int div = n1/n2;
+
+        System.out.println("SUM : "+sum+"\nSUB : "+sub+"\nMUL : "+mul+"\nDIV : "+div);
     }
 }
